a9ca3944b2b4a53250667b0ae3ebe78b5c4dc3cc
ibrahim azouze <ba_tit@ibrahims-MacBook-Air.local>
2024-06-26 18:52:53 +0100

    update the reductiobn service to link with the plan

--- src/services/reductionService.js
+++ src/services/reductionService.js
@@ -1,42 +1,78 @@
-// ibrahim : i had create this file 
+// ibrahim : i had cretate this file 
 const ReductionOffer = require('../models/ReductionOffer');
+const Plan = require('../models/Plan'); // Import the Plan model
 
-//  create a new reduction  
+// create a new reduction
 exports.createReductionOffer = async (data) => {
     try {
         const reductionOffer = new ReductionOffer(data);
-        return await reductionOffer.save();
+        const savedReductionOffer = await reductionOffer.save();
+
+        // Update the corresponding plan
+        await Plan.findByIdAndUpdate(
+            data.plan, // Assuming `data.plan` contains the ID of the related plan
+            { $push: { reductionOffers: savedReductionOffer._id } }
+        );
+
+        return savedReductionOffer;
     } catch (error) {
         throw new Error(error.message);
     }
 };
-// update reduction 
-exports.updateReductionOffer = async (id,data) => {
 
+// update reduction
+exports.updateReductionOffer = async (id, data) => {
     try {
-        const reductionOffer = await ReductionOffer.findByIdAndUpdate(id, data, { new: true });
-        if (!reductionOffer) {
+        const oldReductionOffer = await ReductionOffer.findById(id);
+        if (!oldReductionOffer) {
             throw new Error('ReductionOffer not found');
         }
-        return reductionOffer;
+
+        // Update the reduction offer
+        const updatedReductionOffer = await ReductionOffer.findByIdAndUpdate(id, data, { new: true });
+
+        // If the plan has changed, update the old and new plans
+        if (data.plan && data.plan !== oldReductionOffer.plan.toString()) {
+            // Remove from old plan
+            await Plan.findByIdAndUpdate(
+                oldReductionOffer.plan,
+                { $pull: { reductionOffers: oldReductionOffer._id } }
+            );
+
+            // Add to new plan
+            await Plan.findByIdAndUpdate(
+                data.plan,
+                { $push: { reductionOffers: updatedReductionOffer._id } }
+            );
+        }
+
+        return updatedReductionOffer;
     } catch (error) {
         throw new Error(error.message);
     }
 };
+
 // delete reduction
 exports.deleteReductionOffer = async (id) => {
-
     try {
         const reductionOffer = await ReductionOffer.findByIdAndDelete(id);
         if (!reductionOffer) {
             throw new Error('ReductionOffer not found');
         }
+
+        // Remove from the associated plan
+        await Plan.findByIdAndUpdate(
+            reductionOffer.plan,
+            { $pull: { reductionOffers: reductionOffer._id } }
+        );
+
         return reductionOffer;
     } catch (error) {
         throw new Error(error.message);
     }
 };
-// get reduction by id 
+
+// get reduction by id
 exports.getReductionOfferById = async (id) => {
     try {
         const reductionOffer = await ReductionOffer.findById(id);
@@ -49,14 +85,11 @@ exports.getReductionOfferById = async (id) => {
     }
 };
 
-// get all reduction 
+// get all reduction
 exports.getAllReductionOffers = async () => {
-
     try {
         return await ReductionOffer.find();
     } catch (error) {
         throw new Error(error.message);
     }
 };
-
-
